461dc81327f0b82a4f7684320b29741033ab5ceb
Aldrich Tambajong <aldricht9@gmail.com>
2022-02-19 23:41:54 -0500

    barcode reader

--- test.py
+++ test.py
@@ -1,19 +1,15 @@
-import cv2
-
-cap = cv2.VideoCapture(0)
-
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
-
-while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
-
-    c = cv2.waitKey(1)
-    if c == 27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+def print_even(test_list) :
+    for i in test_list:
+        if i % 2 == 0:
+            yield i
+ 
+# initializing list
+test_list = [1, 4, 5, 6, 7]
+ 
+# printing initial list
+print ("The original list is : " +  str(test_list))
+ 
+# printing even numbers
+print ("The even numbers in list are : ", end = " ")
+for j in print_even(test_list):
+    print (j, end = " ")
--- website/app.py
+++ website/app.py
@@ -1,29 +1,59 @@
-from flask import Flask, Response
+from flask import Flask, Response, redirect, render_template,url_for,make_response
+from pyzbar.pyzbar import decode
 import cv2
+from sqlalchemy import false
 app = Flask(__name__)
-video = cv2.VideoCapture(0)
 
+app.config['SERVER_NAME'] = '0.0.0.0:2204'
 
 @app.route('/')
 def index():
-    return "Default Message"
+    return redirect('/scanner')
 
-
+def read_barcodes(frame):
+    barcodes = decode(frame)
+    for barcode in barcodes:
+        barcode_text = barcode.data.decode('utf-8')
+        return barcode_text
+    return frame
+    
 def gen(video):
     while True:
         success, image = video.read()
         ret, jpeg = cv2.imencode('.jpg', image)
         frame = jpeg.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        text = read_barcodes(image)
+        if type(text) == str:
+            print(type(text))
+            print(text)
+            with app.app_context():
+                print(url_for('after'))
+                return render_template("index.html")
+        else:
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+@app.route('/scanner')
+def scanner():
+    video = cv2.VideoCapture(0)
+    scanner = Response(gen(video),mimetype='multipart/x-mixed-replace; boundary=frame')
+    bypass = False
+    while bypass == False:
+        if gen(video) is True:
+            bypass = True
+        return scanner
+    return redirect('/after')
 
-@app.route('/video_feed')
-def video_feed():
-    global video
-    return Response(gen(video),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    # if gen(video) is False:
+    #     print(gen(video))
+    #     return redirect('/after')
+    # else:
+    #     print(type(gen(video)))
+    #     return Response(gen(video),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/after')
+def after():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2204, threaded=True)
+    app.run(host='0.0.0.0', port=2204, threaded=True, debug=True)
